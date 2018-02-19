64e8bc7464c1613323a22d97b8d1855c8b7330bc
neelshah <neelknightme@gmail.com>
2018-02-19 17:47:29 -0500

    rest api changes set1: main structure

--- core.py
+++ core.py
@@ -1,43 +1,47 @@
-import falcon
-import json
+from flask import Flask
+from flask import jsonify
 import login
 
 # backend of webhealth server
+app = Flask(__name__)
 
-class login_api(object):
-    
-    def on_get(self, req, resp):
-        # This is default statu by User request:
-        resp.status = falcon.HTTP_200
-        result_json = {}
-        try:
-            wh_username = str(req.params['username'])
-            wh_password = str(req.params['password'])
-            result_json = login.login(wh_username,wh_password)
-             
-        except:
-            result_json = {
-                "flag" : False,
-                "Note": "Don't  try any prank. We know what are you doing!"
-                }
-        
-        resp.body = json.dumps(result_json)
-
-class singup_api(object):
-    def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
+@app.route('/login')
+def on_get(self, req, resp):
+    # This is default statu by User request:
+    resp.status = falcon.HTTP_200
+    result_json = {}
+    try:
         wh_username = str(req.params['username'])
         wh_password = str(req.params['password'])
-        result_json = {}
-        try:
-            feedback = login.singup(wh_username, wh_password)
-            result_json = feedback
-        except:
-            result_json = {
-                "flag" : False,
-                "Note" : "Main api error, Sorry!"
+        result_json = login.login(wh_username,wh_password)
+    except:
+        result_json = {
+            "flag" : False,
+            "Note": "Don't  try any prank. We know what are you doing!"
             }
-        resp.body = json.dumps(result_json)
+
+    return jsonify(result_json)
+
+@app.route('/signup')
+def on_get(self, req, resp):
+    resp.status = falcon.HTTP_200
+    wh_username = str(req.params['username'])
+    wh_password = str(req.params['password'])
+    result_json = {}
+    try:
+        feedback = login.singup(wh_username, wh_password)
+        result_json = feedback
+    except:
+        result_json = {
+            "flag" : False,
+            "Note" : "Main api error, Sorry!"
+        }
+    return jsonify(result_json)
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
 app = falcon.API()
 app.add_route('/login', login_api())
 app.add_route('/singup',singup_api())
