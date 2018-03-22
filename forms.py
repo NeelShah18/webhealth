f835dba99617ba7c9d56c4e003bd5d05b3adcfcc
Priyank Sabhaya <priyanksabhaya@gmail.com>
2018-03-22 18:40:59 -0400

    some minor changes in form code

--- forms.py
+++ forms.py
@@ -8,14 +8,14 @@ class SignupForm(Form):
     username = StringField('User Name', validators=[DataRequired("Please enter your username!")])
     email = StringField('Email', validators=[DataRequired("Please enter the email id!"), Email("Please enter valid email address!")])
     password = PasswordField('Password', validators=[DataRequired("Please enter your password!"), Length(min=8, max=16, message="Password should between 8 and 16 charater!")])
-    food = StringField('EFavourite Food', validators=[DataRequired("Please enter your food details!"), Length(min=3, message="Please enter at least one food item!")])
+    food = StringField('Favourite Food', validators=[DataRequired("Please enter your food details!"), Length(min=3, message="Please enter at least one food item!")])
     exer = StringField('Favourite Excercise', validators=[DataRequired("Please enter your excercise details!"), Length(min=2, message="Please enter at least one excersice!")])
     submit = SubmitField("Sign-Up")
 
 class LoginForm(Form):
     email = StringField("Email", validators=[DataRequired("Please enter your email id!"), Email("Please enter valid email address!")])
     password = PasswordField("Password", validators=[DataRequired("Please enter your password!"), Length(min=8, max=16, message="Password is always between 8 and 16 characters!")])
-    submit = SubmitField("Sign in")
+    submit = SubmitField("Sign-In")
 
 class ForgotForm(Form):
     email = StringField("Email", validators=[DataRequired("Please enter your email id!"), Email("Please enter valid email address!")])
